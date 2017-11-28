5ce5ff6994b8fd6b26c8cc641b42430364b14fbb
tborzyszkowski <t.borzyszkowski@gmail.com>
2017-11-28 21:11:41 +0100

    Box View

---
+++ 09SizedBoxView/09SizedBoxView/_09SizedBoxView/SizedBoxViewPage.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace _09SizedBoxView {
+    public class SizedBoxViewPage : ContentPage {
+        public SizedBoxViewPage() {
+            BackgroundColor = Color.Pink;
+
+            Content = new BoxView {
+                Color = Color.Navy,
+                HorizontalOptions = LayoutOptions.Center, // default is Fill
+                VerticalOptions = LayoutOptions.Center,   // default is Fill
+                WidthRequest = 200,  // default is 40 WidthRequest = -1
+                HeightRequest = 100  // default is 40 HeightRequest = -1
+            };
+        }
+    }
+}
