c61063210771b09462c9c953a459e36ddd3c799c
tborzyszkowski <t.borzyszkowski@gmail.com>
2017-12-06 00:12:09 +0100

    Click event & life cycle

---
+++ 15ButtonLogger/15ButtonLogger/_15ButtonLogger/ButtonLoggerPage.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace _15ButtonLogger {
+    public class ButtonLoggerPage : ContentPage {
+        StackLayout loggerLayout = new StackLayout();
+
+        public ButtonLoggerPage() {
+            // Create the Button and attach Clicked handler.
+            Button button = new Button {
+                Text = "Log the Click Time"
+            };
+            button.Clicked += OnButtonClicked;
+
+            Padding = new Thickness(5, Device.RuntimePlatform == Device.iOS ? 20 : 0, 5, 0);
+
+            // Assemble the page.
+            Content = new StackLayout {
+                Children =
+                {
+                    button,
+                    new ScrollView
+                    {
+                        VerticalOptions = LayoutOptions.FillAndExpand,
+                        Content = loggerLayout
+                    }
+                }
+            };
+        }
+
+        void OnButtonClicked(object sender, EventArgs args) {
+            // Add Label to scrollable StackLayout.
+            loggerLayout.Children.Add(new Label {
+                Text = "Button clicked at " + DateTime.Now.ToString("T")
+            });
+        }
+    }
+}
---
+++ 19PersistentKeypad/19PersistentKeypad/_19PersistentKeypad/App.xaml.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace _19PersistentKeypad {
+    public partial class App : Application {
+        const string displayLabelText = "displayLabelText";
+        public App() {
+            if (Properties.ContainsKey(displayLabelText)) {
+                DisplayLabelText = (string)Properties[displayLabelText];
+            }
+
+            InitializeComponent();
+
+            MainPage = new _19PersistentKeypad.PersistentKeypadPage();
+        }
+        public string DisplayLabelText { set; get; }
+
+        protected override void OnStart() {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep() {
+            // Handle when your app sleeps
+            Properties[displayLabelText] = DisplayLabelText;
+        }
+
+        protected override void OnResume() {
+            // Handle when your app resumes
+        }
+    }
+}
