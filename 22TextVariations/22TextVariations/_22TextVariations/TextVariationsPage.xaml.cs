ea580d63414ef4fcced2211b39d9b247df07a5d5
tborzyszkowski <t.borzyszkowski@gmail.com>
2017-12-12 20:17:40 +0100

    XAML

---
+++ 22TextVariations/22TextVariations/_22TextVariations/TextVariationsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _22TextVariations {
+    public partial class TextVariationsPage : ContentPage {
+        public TextVariationsPage() {
+            InitializeComponent();
+        }
+    }
+}
