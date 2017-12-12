562a8fcabd649c2bb4538d4f2136e49d8c4a16a1
tborzyszkowski <t.borzyszkowski@gmail.com>
2017-12-12 23:29:10 +0100

    More XAML

---
+++ 23Params/23Params/_23Params/ParamsPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace _23Params {
+    public partial class ParamsPage : ContentPage {
+        public ParamsPage() {
+            InitializeComponent();
+        }
+    }
+}
---
+++ 26XamlKeypad/26XamlKeypad/_26XamlKeypad/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+using Xamarin.Forms;
+
+namespace _26XamlKeypad {
+     public partial class App : Application
+    {
+        const string displayLabelText = "displayLabelText";
+
+        public App()
+        {
+            if (Properties.ContainsKey(displayLabelText))
+            {
+                DisplayLabelText = (string)Properties[displayLabelText];
+            }
+
+            MainPage = new XamlKeypadPage();
+        }
+
+        public string DisplayLabelText { set; get; }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+            Properties[displayLabelText] = DisplayLabelText;
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
