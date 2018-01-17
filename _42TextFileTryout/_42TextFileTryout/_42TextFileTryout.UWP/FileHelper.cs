b7f83171d52b073a89557f87ba280bae71c365a7
tborzyszkowski <t.borzyszkowski@gmail.com>
2018-01-17 01:15:00 +0100

    FileAsync

---
+++ _42TextFileTryout/_42TextFileTryout/_42TextFileTryout.UWP/FileHelper.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+using _42TextFileTryout;
+
+[assembly: Dependency(typeof(_42TextFileTryout.UWP.FileHelper))]
+
+namespace _42TextFileTryout.UWP
+{
+    class FileHelper : IFileHelper
+    {
+        public bool Exists(string filename)
+        {
+            return false;
+        }
+
+        public void WriteText(string filename, string text)
+        {
+            throw new NotImplementedException("Writing files is not implemented");
+        }
+
+        public string ReadText(string filename)
+        {
+            throw new NotImplementedException("Reading files is not implemented");
+        }
+
+        public IEnumerable<string> GetFiles()
+        {
+            return new string[0];
+        }
+
+        public void Delete(string filename)
+        {
+        }
+    }
+}
